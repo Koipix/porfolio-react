1475292d93c5182636b32a2cb9b4362ea50d5458
Koi <92094832+Koipix@users.noreply.github.com>
2025-02-27 23:24:52 +0800

    React Replacement Finished
    Ready to work on project section

--- src/compoenents/Content.tsx
+++ src/compoenents/Content.tsx
@@ -1,6 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubAlt, faSquareXTwitter, faItchIo  } from '@fortawesome/free-brands-svg-icons'
+
 import Typed from 'typed.js';
 import './Content.css';
+import rika from '../assets/mashiro.png';
 
 const Content = () => {
   const typedRef = useRef(null);
@@ -20,16 +24,34 @@ const Content = () => {
   }, []);
 
   return (
-    <div className='content'>
-        <h1>Hello! My name is</h1>
-        <h2>Koi</h2>
-        <h3>
-            I'm pursuing for: 
-            <br/>
-            .goals(
-            <span ref={typedRef} className="auto-type"></span>
-            )
-        </h3>
+    <div className="main">
+        <div className='content'>
+            <h1>Hello! My name is</h1>
+            <h2>Koi</h2>
+            <h3>
+                I'm pursuing for:
+                <br/>
+                .goals(
+                <span ref={typedRef} className="auto-type"></span>
+                )
+            </h3>
+        </div>
+
+        <div className="socials">
+            <a href="https://github.com/Koipix" target="_blank">
+                <FontAwesomeIcon icon={faGithubAlt} className='icon'/>
+            </a>
+            <a href='https://koipix.itch.io/' target='_blank'>
+                <FontAwesomeIcon icon={faItchIo} className='icon'/>
+            </a>
+            <a href="#">
+                <FontAwesomeIcon icon={faSquareXTwitter} className='icon'/>
+            </a>
+        </div>
+
+        <div className="img-box">
+            <img src={rika} alt="rika" className="hook"/>
+        </div>
     </div>
   );
 };
--- src/compoenents/Nav.tsx
+++ src/compoenents/Nav.tsx
@@ -1,17 +1,32 @@
-import React from 'react'
-import './Nav.css'
+import { useEffect, useRef } from 'react';
+import './Nav.css';
 
 const Nav = () => {
+
+    const navRef = useRef<HTMLElement | null>(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (navRef.current) {
+                navRef.current.classList.toggle("sticky", window.scrollY > 0);
+            }
+        }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+
+    }, []);
+
   return (
-    <div className='navbar'>
+    <nav ref={navRef} className='navbar'>
       <h1 className='logo'>koi.<span>pix</span>()</h1>
 
       <ul>
-        <li><a href="#">.home()</a></li>
+        <li><a href="#" className="active">.home()</a></li>
         <li><a href="#">.aboutme()</a></li>
         <li><a href="#">.projects()</a></li>
       </ul>
-    </div>
+    </nav>
   )
 }
 
