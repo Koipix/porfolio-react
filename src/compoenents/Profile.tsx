d334479af8bdac27cb9b6bcd9ce873063921ed7c
Koipix <koi.devt@gmail.com>
2025-04-12 23:58:28 +0800

    @layer fix

--- src/compoenents/Profile.tsx
+++ src/compoenents/Profile.tsx
@@ -1,7 +1,18 @@
 const Profile = () => {
     return (
-        <div>
-            <h1>Hello, World!</h1>
+        <div className="flex justify-center">
+            <div className="w-4/5 mg">
+                <header>
+                    <a href="https://github.com/koipix" className="inline-flex items-center">
+                        <img src="https://avatars.githubusercontent.com/u/92094832?v=4" 
+                             alt="pfp"
+                             className="h-24 rounded-xl mr-8"/>
+                        <span className="text-white text-2xl">
+                            Koi :3
+                        </span>
+                    </a>
+                </header>
+            </div>
         </div>
     )
 }
